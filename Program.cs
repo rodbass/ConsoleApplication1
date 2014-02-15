bfe600701070ad46a7303ea657bc7bc7065751a6
rodbass <rodbass84@hotmail.com>
2014-02-15 16:59:43 -0500

    First Git project

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var @this = new Program();
+            try
+            {
+                @this.Test();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.WriteLine("\n\n\nPress any key to continue...");
+                Console.ReadKey();
+            }
+        }
+
+        void Test()
+        {
+        }
+    }
+}
